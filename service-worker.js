8104a65667f860dd83cb5fdda512d9ae7b2e1a25
celsojutcel-eng <celso.jutcel@gmail.com>
2026-01-07 09:31:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'projeto-fenix-v2';
-const FILES_TO_CACHE = [
+const CACHE_NAME='projeto-fenix-final';
+const FILES_TO_CACHE=[
   './',
   './index.html',
   './manifest.json',
@@ -7,30 +7,18 @@ const FILES_TO_CACHE = [
   './icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', event=>{
   console.log('[SW] Instalando...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', event=>{
   console.log('[SW] Ativando...');
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener('fetch', event=>{
+  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
 });
